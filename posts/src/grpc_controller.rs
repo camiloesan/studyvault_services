453bb639039cb67d36f721fe1856411e980b09a2
camilo <camiloesan@gmail.com>
2024-10-26 20:20:21 -0600

    proto updated

--- posts/src/grpc_controller.rs
+++ posts/src/grpc_controller.rs
@@ -1,8 +1,8 @@
-use crate::posts::posts::PostInfo;
+use crate::posts::posts_response::PostInfo;
 use crate::posts::posts_service_server::PostsService;
-use crate::posts::Channel;
-use crate::posts::Posts;
-use crate::posts::{FileChunk, UploadStatus};
+use crate::posts::ChannelRequest;
+use crate::posts::PostsResponse;
+use crate::posts::{FileChunk, UploadStatusResponse};
 use crate::sql_operations;
 use tokio::io::AsyncWriteExt;
 use tonic::{Request, Response, Status};
@@ -16,7 +16,7 @@ impl PostsService for PostsServicesStruct {
     async fn upload_post(
         &self,
         request: Request<tonic::Streaming<FileChunk>>,
-    ) -> Result<Response<UploadStatus>, Status> {
+    ) -> Result<Response<UploadStatusResponse>, Status> {
         println!("upload requested");
         let mut stream = request.into_inner();
         let mut file = None;
@@ -72,7 +72,7 @@ impl PostsService for PostsServicesStruct {
         )
         .await;
 
-        Ok(Response::new(UploadStatus {
+        Ok(Response::new(UploadStatusResponse {
             success: result,
             message: format!("File uploaded successfully"),
         }))
@@ -80,8 +80,8 @@ impl PostsService for PostsServicesStruct {
 
     async fn get_posts_by_channel_id(
         &self,
-        request: Request<Channel>,
-    ) -> Result<Response<Posts>, Status> {
+        request: Request<ChannelRequest>,
+    ) -> Result<Response<PostsResponse>, Status> {
         let channel_id = request.into_inner().channel_id;
 
         let db_posts = sql_operations::get_posts_by_channel_id(channel_id).await;
@@ -98,7 +98,7 @@ impl PostsService for PostsServicesStruct {
             })
             .collect();
 
-        let response = Posts { posts: post_infos };
+        let response = PostsResponse { posts: post_infos };
 
         Ok(Response::new(response))
     }
